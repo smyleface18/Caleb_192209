e0b1cfe62bf16e9a7c8a3338667c634e84b0bee2
smyleface18 <ca30850@gmail.com>
2023-11-27 22:53:15 -0500

    terminado :D

---
+++ js/login.js
@@ -0,0 +1,8 @@
+const button = document.querySelector("#submit")
+
+console.log(button);
+
+button.addEventListener("click",()=>{
+    console.log("hola");
+    window.location.href = "suzuki.html";
+});
--- js/loguaer.js
+++
@@ -1,7 +0,0 @@
-const buttom  = document.getElementById("submit");
-
-console.log(buttom)
-
-buttom.addEventListener("click",()=>{
-    window.location.assign("html/Principal.html");
-})
